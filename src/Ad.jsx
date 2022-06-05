fa2b492d4b4c162dec95c97be64ab2a32f5164e1
Roman <spiderr.roma@gmail.com>
2022-06-05 19:03:57 +0300

    flag changes depending on country

--- src/Ad.jsx
+++ src/Ad.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Ad.css'
-
+import useGeoLocation from 'react-ipgeolocation'
 const username = "Andriy"
 
 //міняти ці переменні в 19 строчці
@@ -10,16 +10,58 @@ const COUNTRY_RUSSIA = "РОСІЇ"
 
 const ukr = "https://hromadske.ua/"
 const rus = "https://www.currenttime.tv/"
+const german = "https://www.dw.com/"
+const israel = "https://www.timesofisrael.com/"
+const au = "https://www.theaustralian.com.au/"
+
 
 const Ad = () => {
-  return (
-    <div className='ad'>
+  const location = useGeoLocation();
+
+  if (location.country === 'UA') {
+    return (
+      <div className='ad'>
       <a href={ukr}></a>
-    <h3>ХОЧЕШ ЗНАТИ 
-    УСЮ НОВУ ІНФОРМАЦІЮ 
-    З {COUNTRY_UKRAINE}</h3>
-    </div>
-  )
+      <h3>ХОЧЕШ ЗНАТИ 
+      УСЮ НОВУ ІНФОРМАЦІЮ 
+       З {COUNTRY_UKRAINE}</h3>
+      </div>
+    )
+  } else if (location.country === 'DE') {
+    return (
+      <div className='ad rus'>
+      <a href={german}></a>
+      <h3>WANT TO KNOW
+      NEW INFORMATION 
+      FROM GERMANY</h3>
+      </div>
+    )
+  } else if (location.country === 'AU') {
+    return (
+    <div className='ad au'>
+      <a href={au}></a>
+      <h3>WANT TO KNOW
+      NEW INFORMATION 
+      FROM AUSTRALIA</h3>
+      </div>
+    )
+  } else if(location.country === 'IL' || 'IT') {
+    return (
+      <div className='ad is'>
+      <a href={israel}></a>
+      <h3>WANT TO KNOW
+      NEW INFORMATION 
+      FROM ISRAEL</h3>
+      </div>
+    )
+  }
+  return 
+    // <div className='ad'>
+    //   <a href={ukr}></a>
+    // <h3>ХОЧЕШ ЗНАТИ 
+    // УСЮ НОВУ ІНФОРМАЦІЮ 
+    // З {COUNTRY_UKRAINE}</h3>
+    // </div>
 }
 
 export default Ad;
